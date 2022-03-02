aee021f9f400657ecd1e915b5d5ac6bbedc05289
w13772027411 <yang_wang@asdc.com.cn>
2022-03-02 16:39:19 +0800

    提交初始化

---
+++ src/main/java/Jsvideo.java
@@ -0,0 +1,17 @@
+package ;
+import .*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.time.LocalDateTime;
+public class Jsvideo {
+private String  title;
+private String  time_update;
+private List<String > hot_id;
+private String  time_create;
+private String  _id;
+private String  time;
+private Integer  type;
+private List<String > video_list;
+private Integer  status;
+}
